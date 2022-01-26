2ccd25ecc0c21bf4babcfd3270bf42c5229d9eda
rhkearns <ryankearns217@gmail.com>
2022-01-26 16:06:49 -0500

    make fake data

---
+++ fakeData.js
@@ -0,0 +1,37 @@
+export const UserList = [
+  {
+    id: 1,
+    name: "John",
+    username: "john",
+    age: 20,
+    nationality: "Canada"
+  },
+  {
+    id: 2, 
+    name: 'Pedro',
+    username: "PedroTech",
+    age: 20,
+    nationality: "Brazil",
+  },
+  {
+    id: 3,
+    name: "Sarah",
+    username: "cameron",
+    age: 25,
+    nationality: "United States",
+  },
+  {
+    id: 4,
+    name: "Rafe",
+    username: "refe123",
+    age: 60,
+    nationality: "Germany",
+  },
+  {
+    id: 5,
+    name: "Kelly",
+    username: "kelly2019",
+    age: 5,
+    nationality: "Chile",
+  },
+]
