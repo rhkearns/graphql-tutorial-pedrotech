b6fd535fcb5e766861e85e58e38ba55feed6eaaf
rhkearns <ryankearns217@gmail.com>
2022-01-27 16:03:23 -0500

    delete mutation functional

--- schema/type-defs.js
+++ schema/type-defs.js
@@ -7,48 +7,49 @@ const typeDefs = gql`
 		username: String!
 		age: Int!
 		nationality: Nationality!
-    friends: [User]
-    favoriteMovies: [Movie]
+		friends: [User]
+		favoriteMovies: [Movie]
 	}
 
-  type Movie {
-    id: ID!
-    name: String!
-    year: Int!
-    isInTheaters: Boolean!
-  }
+	type Movie {
+		id: ID!
+		name: String!
+		year: Int!
+		isInTheaters: Boolean!
+	}
 
 	type Query {
 		users: [User!]!
-    user(id: ID!): User!
-    movies: [Movie!]!
-    movie(name: String!): Movie!
+		user(id: ID!): User!
+		movies: [Movie!]!
+		movie(name: String!): Movie!
 	}
 
-  input CreateUserInput {
-    name: String!
+	input CreateUserInput {
+		name: String!
 		username: String!
 		age: Int!
 		nationality: Nationality = BRAZIL
-  }
+	}
 
-  input UpdateUsernameInput {
-    id: ID!
-    newUsername: String!
-  }
+	input UpdateUsernameInput {
+		id: ID!
+		newUsername: String!
+	}
 
-  type Mutation {
-    createUser(input: CreateUserInput!): User
-    updateUsername(input: UpdateUsernameInput!): User
-  }
+	type Mutation {
+		createUser(input: CreateUserInput!): User
+		updateUsername(input: UpdateUsernameInput!): User
+		deleteUser(id: ID!): User
+	}
 
-  enum Nationality {
-    CANADA
-    BRAZIL
-    INDIA
-    GERMANY
-    CHILE
-  }
+	enum Nationality {
+		CANADA
+		BRAZIL
+		INDIA
+		GERMANY
+		CHILE
+	}
 `;
 
 module.exports = { typeDefs };
